9a852c2897c4f71346c5d05ed810012acb178ad9
huxins <huxins@163.com>
2022-05-11 21:28:47 +0800

    123

--- docs/.vuepress/config/tools.js
+++ docs/.vuepress/config/tools.js
@@ -2,14 +2,13 @@ const maven = [
     '/tools/maven/maven-environment-setup',
     '/tools/maven/maven-settings',
     '/tools/maven/maven-pom',
-    '/tools/maven/maven-build-lifecycle',
-    '/tools/maven/maven-build-profiles',
-    '/tools/maven/maven-plugins',
-    '/tools/maven/maven-resources',
     '/tools/maven/maven-properties',
     '/tools/maven/maven-dependencies',
-    '/tools/maven/maven-archetypes',
-    '/tools/maven/maven-dependencies'
+    '/tools/maven/maven-resources',
+    '/tools/maven/maven-build-lifecycle',
+    '/tools/maven/maven-plugins',
+    '/tools/maven/maven-build-profiles',
+    '/tools/maven/maven-archetypes'
 ]
 
 const cms = [
