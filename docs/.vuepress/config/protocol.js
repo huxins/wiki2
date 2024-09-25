5acca565e5afb99df34d69176a958b8a85c64de9
huxins <huxins@163.com>
2024-09-25 23:17:17 +0800

    http

--- docs/.vuepress/config/protocol.js
+++ docs/.vuepress/config/protocol.js
@@ -4,12 +4,9 @@ const generic_syntax = [
 ]
 
 const http = [
-    '/protocol/http/http-specifications',
-    '/protocol/http/http-message',
     '/protocol/http/http-semantics-and-content',
     '/protocol/http/http-range-requests',
     '/protocol/http/http-caching',
-    '/protocol/http/http-headers',
     '/protocol/http/http-connections',
     '/protocol/http/http-cookies',
     '/protocol/http/http-status-code'
